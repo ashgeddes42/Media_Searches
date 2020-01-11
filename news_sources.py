0f4ad572ef8a2b5764f0b1885e3ba422134979fe
ashgeddes42 <46552508+ashgeddes42@users.noreply.github.com>
2020-01-11 21:04:00 +1100

    Update news_sources.py

--- news_sources.py
+++ news_sources.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import requests
 import json
@@ -8,23 +7,9 @@ app = Flask(__name__)
 
 url = 'https://newsapi.org/v2/top-headlines?sources={query}&apiKey=2f722a1876b34f38bfd0fe5558ee006a&q'
 
-'''
-def store_url(key):
-  return f'https://store.ncss.cloud/{code}/{key}'
-
-def store_get(key):
-  url = store_url(key)
-  r = request.get(key)
-  r= request.get(url)
-  if r.status_code == 404:
-    return {}
-  else:
-    return r.get_json()
-
-'''
 
 #https://RundownGroundedServers--five-nine.repl.co/syd2/news
-@app.route("/syd2/news_source", methods=['POST'])
+@app.route("/syd-2/news_source", methods=['POST'])
 def news():
   #GET the query / what they want to here about
   args = request.get_json()
@@ -33,21 +18,23 @@ def news():
   #Trigger the API with the REQUEST
   response = requests.get(url.format(query=query)).json()
   #GET the TITLE and AUTHOR from the response
-
+  print(response)
   title = response['articles'][0]['title']
   author = response['articles'][0]['author']
   description = response['articles'][0]['description']
   image_url = response['articles'][0]['urlToImage']
   article_url = response['articles'][0]['url']
   time = response['articles'][0]['publishedAt']
+  date = time[:10]
+  date = date.replace('-','/')
 
   #ASSEMBLE the REPLY
-  reply =  f'<p style="font-weight:bold;font-family:times new roman;font-size: 20px"><a href = "{article_url}">{title}</a></p> <p>{news} </p><p style="color:red;font-family:times new roman">Written by {author} </p> <p>{time}</p><p><img src="{image_url}" alt="" align="middle" style="width:800px"></p><p style = "font-family:times new roman">{description}</p>' 
+  reply =  f'<p style="font-weight:bold;font-family:times new roman;font-size: 20px"><a href = "{article_url}">{title}</a></p> <p>{news} </p><p style="color:red;font-family:times new roman">Written by {author} </p> <p> Posted on {date}</p><p><img src="{image_url}" alt="" align="middle" style="width:800px"></p><p style = "font-family:times new roman">{description}</p>' 
 
 
   #GIVE the REPLY back to USER
   return jsonify({
-    'author': 'News Bot',
+    'author': 'Alfred',
     'text': reply,
   })
 
